6cbd6e82d9fa84039004bf34d9d694d728459df7
Anubhav Mishra <anubhav.mishra@students.iiit.ac.in>
2024-12-28 19:27:27 +0530

    Update api.py
    updated filenames by timestamps

--- api.py
+++ api.py
@@ -2,6 +2,7 @@ from flask import Flask, request, send_file
 from flask_cors import CORS
 import subprocess
 import os
+import time
 
 app = Flask(__name__)
 CORS(app)
@@ -12,16 +13,24 @@ def seamcarve():
     n_of_cols_to_reduce = int(request.form['n_of_cols_to_reduce'])
     n_of_rows_to_reduce = int(request.form['n_of_rows_to_reduce'])
 
-    
+    # Generate unique filenames
+    timestamp = str(int(time.time()))
+    input_filename = f'input_{timestamp}.jpg'
+    output_filename = f'output_{timestamp}.jpg'
+
     # Save the image
-    file.save('input.jpg')
+    file.save(input_filename)
     
     # Call the C++ executable
-    subprocess.run(['./seamcarving', 'input.jpg', 'output.jpg', str(n_of_cols_to_reduce), str(n_of_rows_to_reduce)])
-
-    os.remove('input.jpg')
+    subprocess.run(['./seamcarving', input_filename, output_filename, str(n_of_cols_to_reduce), str(n_of_rows_to_reduce)])
     
-    return send_file('output.jpg', mimetype='image/jpeg')
+    response = send_file(output_filename, mimetype='image/jpeg')
+    
+    # Delete the input and output files after processing
+    os.remove(input_filename)
+    os.remove(output_filename)
+    
+    return response
 
 if __name__ == '__main__':
     app.run(debug=True)
